222e70c8f623dacaa3864b7398cf3caf9ee27a94
BipinKalra <bipinkalra@gmail.com>
2020-10-07 19:40:36 +0530

    script removed

--- script.js
+++
@@ -1,9 +0,0 @@
-var data;
-
-$.get('data/news.json', function(d) {
-  data = JSON.parse(d);
-  data.forEach(function(news) {
-    // $("#books").append(`<div><h2>${news.title}</h2><p>${news.author}</p></div>`);
-    console.log(news)
-  });
-});

9d75b2de3da418e1e605a3138f547efc7c9312d8
BipinKalra <bipinkalra@gmail.com>
2020-10-07 23:29:56 +0530

    json data

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,6 +3,10 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
+const _061798d3 = () => interopDefault(import('../pages/community/index.vue' /* webpackChunkName: "pages/community/index" */))
+const _0310f427 = () => interopDefault(import('../pages/company/index.vue' /* webpackChunkName: "pages/company/index" */))
+const _b16c27ec = () => interopDefault(import('../pages/contact/index.vue' /* webpackChunkName: "pages/contact/index" */))
+const _57249e45 = () => interopDefault(import('../pages/events/index.vue' /* webpackChunkName: "pages/events/index" */))
 const _337eea39 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 // TODO: remove in Nuxt 3
@@ -22,6 +26,22 @@ export const routerOptions = {
   scrollBehavior,
 
   routes: [{
+    path: "/community",
+    component: _061798d3,
+    name: "community"
+  }, {
+    path: "/company",
+    component: _0310f427,
+    name: "company"
+  }, {
+    path: "/contact",
+    component: _b16c27ec,
+    name: "contact"
+  }, {
+    path: "/events",
+    component: _57249e45,
+    name: "events"
+  }, {
     path: "/",
     component: _337eea39,
     name: "index"

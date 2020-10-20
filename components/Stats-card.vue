cb08d58ec84e93964583bf444084f5c398242751
BipinKalra <bipinkalra@gmail.com>
2020-10-20 17:42:54 +0530

    components 1

---
+++ components/Stats-card.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="bg-grey-dark-1 br-5 p-30">
+    <div class="row no-gutters align-items-center">
+      <img
+        src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/workshops.svg"
+        class="mb-sm-3"
+        style="height: 50px"
+      />
+      <div class="flex-1 d-sm-none d-block pl-20">
+        <div
+          class="text-gradient-orange-light bold"
+          style="font-size: 3.428rem"
+        >
+          40K+
+        </div>
+        <div class="mt-1 font-4">Students Taught</div>
+      </div>
+    </div>
+    <div class="d-sm-block d-none">
+      <div class="text-gradient-orange-light bold" style="font-size: 3.428rem">
+        40K+
+      </div>
+      <div class="mt-1 font-4">Students Taught</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>

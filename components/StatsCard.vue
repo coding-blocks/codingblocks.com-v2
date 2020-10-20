243ee30c6c264554ee4491b89ae9e2c9beafc257
BipinKalra <bipinkalra@gmail.com>
2020-10-20 17:58:37 +0530

    component in index

--- components/StatsCard.vue
+++ components/StatsCard.vue
@@ -2,7 +2,7 @@
   <div class="bg-grey-dark-1 br-5 p-30">
     <div class="row no-gutters align-items-center">
       <img
-        src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/workshops.svg"
+        :src="{ icon }"
         class="mb-sm-3"
         style="height: 50px"
       />
@@ -11,20 +11,22 @@
           class="text-gradient-orange-light bold"
           style="font-size: 3.428rem"
         >
-          40K+
+          {{ stat_number }}
         </div>
-        <div class="mt-1 font-4">Students Taught</div>
+        <div class="mt-1 font-4">{{ stat_title }}</div>
       </div>
     </div>
     <div class="d-sm-block d-none">
       <div class="text-gradient-orange-light bold" style="font-size: 3.428rem">
-        40K+
+        {{ stat_number }}
       </div>
-      <div class="mt-1 font-4">Students Taught</div>
+      <div class="mt-1 font-4">{{ stat_title }}</div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: [ "stat_number", "stat_title", "icon" ]
+};
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,13 @@
 <template>
   <h1>HEADING</h1>
 </template>
+
+<script>
+import StatsCard from "@/components/StatsCard.vue"
+
+export default {
+  components: {
+    StatsCard
+  }
+}
+</script>

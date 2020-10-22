1fce71ccbc397bd33cf8a4e393837d50815d2aa4
BipinKalra <bipinkalra@gmail.com>
2020-10-22 18:27:39 +0530

    shift

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,5 +1,5 @@
-export { default as StatsCard } from '../../components/home/StatsCard.vue'
 export { default as WelcomeSection } from '../../components/home/WelcomeSection.vue'
+export { default as StatsCard } from '../../components/misc/StatsCard.vue'
 
-export const LazyStatsCard = import('../../components/home/StatsCard.vue' /* webpackChunkName: "components/home/StatsCard" */).then(c => c.default || c)
 export const LazyWelcomeSection = import('../../components/home/WelcomeSection.vue' /* webpackChunkName: "components/home/WelcomeSection" */).then(c => c.default || c)
+export const LazyStatsCard = import('../../components/misc/StatsCard.vue' /* webpackChunkName: "components/misc/StatsCard" */).then(c => c.default || c)

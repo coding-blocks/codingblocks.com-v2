8fc34f15b826938d3fd77537819dbf436c98dfb3
BipinKalra <bipinkalra@gmail.com>
2020-10-22 16:43:19 +0530

    component 1

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,50 @@
 <template>
-
+  <div class="a-cbm">
+    <div class="landing-page">
+      <!-- Welcome Section -->
+      <div class="landing-page__section">
+        <div class="row align-items-center">
+          <div class="col-xl-6 col-lg-5 mb-lg-0 mb-50">
+            <div class="heading-font stroke-font text-grey-dark-2">
+              WELCOME TO
+            </div>
+            <div class="heading-font">CODING BLOCKS</div>
+            <img
+              src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/underline_welcome.svg"
+            />
+            <div class="heading-2 bold my-50">
+              India’s best technology learning platform.
+            </div>
+            <button class="button-primary">
+              Get Started
+              <img
+                src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/button-icon.svg"
+                class="ml-2"
+              />
+            </button>
+          </div>
+          <div class="col-xl-6 col-lg-7">
+            <div class="row">
+              <div class="col-sm-6 mb-sm-0 mb-30">
+                <StatsCard
+                  stat_number="40K+"
+                  stat_title="Students Taught"
+                  icon="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/workshops.svg"
+                />
+                <div class="mt-30"></div>
+                <StatsCard />
+              </div>
+              <div class="col-sm-6 pt-sm-30">
+                <!-- <StatsCard /> -->
+                <div class="mt-30"></div>
+                <!-- <StatsCard /> -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>

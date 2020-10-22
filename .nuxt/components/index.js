fa1f41f119f7f612cd6ddd083be841b0f8e24817
BipinKalra <bipinkalra@gmail.com>
2020-10-22 18:27:16 +0530

    nested component for welcome section

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,3 +1,5 @@
 export { default as StatsCard } from '../../components/home/StatsCard.vue'
+export { default as WelcomeSection } from '../../components/home/WelcomeSection.vue'
 
 export const LazyStatsCard = import('../../components/home/StatsCard.vue' /* webpackChunkName: "components/home/StatsCard" */).then(c => c.default || c)
+export const LazyWelcomeSection = import('../../components/home/WelcomeSection.vue' /* webpackChunkName: "components/home/WelcomeSection" */).then(c => c.default || c)
---
+++ components/misc/StatsCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="bg-grey-dark-1 br-5 p-30">
+    <div class="row no-gutters align-items-center">
+      <img :src="icon" class="mb-sm-3" style="height: 50px" />
+      <div class="flex-1 d-sm-none d-block pl-20">
+        <div
+          class="text-gradient-orange-light bold"
+          style="font-size: 3.428rem"
+        >
+          {{ stat_number }}
+        </div>
+        <div class="mt-1 font-4">{{ stat_title }}</div>
+      </div>
+    </div>
+    <div class="d-sm-block d-none">
+      <div class="text-gradient-orange-light bold" style="font-size: 3.428rem">
+        {{ stat_number }}
+      </div>
+      <div class="mt-1 font-4">{{ stat_title }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["stat_number", "stat_title", "icon"],
+};
+</script>

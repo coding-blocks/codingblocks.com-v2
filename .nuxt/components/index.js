003637eccc2c5baa52cd5795403978b7470e1c1f
BipinKalra <bipinkalra@gmail.com>
2020-10-26 20:42:34 +0530

    eventscard

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,7 +1,9 @@
 export { default as WelcomeSection } from '../../components/home/WelcomeSection.vue'
 export { default as CardCarousel } from '../../components/misc/CardCarousel.vue'
+export { default as EventsCard } from '../../components/misc/EventsCard.vue'
 export { default as StatsCard } from '../../components/misc/StatsCard.vue'
 
 export const LazyWelcomeSection = import('../../components/home/WelcomeSection.vue' /* webpackChunkName: "components/home/WelcomeSection" */).then(c => c.default || c)
 export const LazyCardCarousel = import('../../components/misc/CardCarousel.vue' /* webpackChunkName: "components/misc/CardCarousel" */).then(c => c.default || c)
+export const LazyEventsCard = import('../../components/misc/EventsCard.vue' /* webpackChunkName: "components/misc/EventsCard" */).then(c => c.default || c)
 export const LazyStatsCard = import('../../components/misc/StatsCard.vue' /* webpackChunkName: "components/misc/StatsCard" */).then(c => c.default || c)
---
+++ components/misc/EventsCard.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="card card--dark">
+    <div class="card__header card__header--border-highlight">
+      <div class="flex-1">
+        <div class="heading-6">WORKSHOP</div>
+      </div>
+      <div class="t-align-c">
+        <div class="heading-4 bold">12</div>
+        <div class="font-4">MAR</div>
+      </div>
+    </div>
+    <div>
+      <div class="heading-5 text-orange-dark-1 bold mb-10">
+        Machine Learning
+      </div>
+      <div class="font-4">
+        Learn how to build apps like spotify, netflix and more.
+      </div>
+    </div>
+    <div class="mentor-row my-50">
+      <div class="mentor-row__text-container align-self-center pr-100">
+        <div>MENTOR</div>
+        <div class="font-5 bold mt-5">Arnav G, Prateek N</div>
+      </div>
+      <div
+        class="mentor-row__image-container mentor-row__image-container--mentor-2"
+      >
+        <img
+          src="https://minio.codingblocks.com/amoeba/4078be4e-72e1-451a-9c4e-9223d2bbcc2c"
+          alt="photo ofRishab Kapoor"
+          class="mentor-row__image-container__mentor-image s-80x80"
+        />
+        <img
+          src="https://minio.codingblocks.com/amoeba/4078be4e-72e1-451a-9c4e-9223d2bbcc2c"
+          alt="photo ofRishab Kapoor"
+          class="mentor-row__image-container__mentor-image s-80x80"
+        />
+      </div>
+    </div>
+    <div class="card__footer card__footer--border-highlight">
+      <div class="flex-1">
+        <div class="font-4">8:00 PM Onwards</div>
+      </div>
+      <div class="button-tertiary">
+        Register Now
+        <img
+          src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/button-icon-orange.svg"
+          class="ml-2"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    "event_type",
+    "event_date",
+    "mentor_names",
+    "mentor_logo_1",
+    "mentor_logo_2",
+    "event_time"
+  ],
+};
+</script>

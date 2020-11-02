7a0f4653214047fed3a87a1b9363fc851eaed8c1
BipinKalra <bipinkalra@gmail.com>
2020-11-02 18:41:34 +0530

    named slots implementation

--- pages/index.vue
+++ pages/index.vue
@@ -133,99 +133,93 @@
       </div>
 
       <!-- Community Section -->
-      <div class="landing-page__section landing-page__section--divided-section">
-        <div class="row c-card-carousel-lg">
-          <div
-            class="col-lg-6 col-12 landing-page__section--divided-section__left-section"
-          >
-            <div>
-              <div class="heading-font stroke-font text-grey-dark-2">
-                JOIN OUR
+      <DividedSection>
+        <template v-slot:left_section>
+          <div>
+            <div class="heading-font stroke-font text-grey-dark-2">
+              JOIN OUR
+            </div>
+            <div class="heading-font">COMMUNITY</div>
+            <img
+              src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/underline_community.svg"
+            />
+            <div class="my-50">
+              <div class="heading-2 bold">
+                Coding: The present and future
               </div>
-              <div class="heading-font">COMMUNITY</div>
-              <img
-                src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/underline_community.svg"
-              />
-              <div class="my-50">
-                <div class="heading-2 bold">
-                  Coding: The present and future
-                </div>
-                <div class="mt-2 text-yellow heading-4">
-                  Connect with like minded people.
-                </div>
+              <div class="mt-2 text-yellow heading-4">
+                Connect with like minded people.
               </div>
             </div>
-            <div
-              class="row no-gutters align-items-center d-xl-flex d-lg-none d-sm-flex d-none"
+          </div>
+          <div
+            class="row no-gutters align-items-center d-xl-flex d-lg-none d-sm-flex d-none"
+          >
+            <button
+              class="button-secondary flex-1"
+              style="max-width: fit-content"
             >
-              <button
-                class="button-secondary flex-1"
-                style="max-width: fit-content"
-              >
-                <div class="flex-1 t-align-l">Visit Youtube channel</div>
-                <img
-                  src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/youtube_button.svg"
-                  class="ml-2"
-                />
-              </button>
-              <button
-                class="button-secondary ml-4 flex-1"
-                style="max-width: fit-content"
-              >
-                <div class="flex-1 t-align-l">Join Telegram channel</div>
-                <img
-                  src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/telegram_button.svg"
-                  class="ml-2"
-                />
-              </button>
-            </div>
-            <div class="d-xl-none d-lg-block d-sm-none d-block">
-              <button class="button-secondary">
-                Visit Youtube channel
-                <img
-                  src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/youtube_button.svg"
-                  class="ml-2"
-                />
-              </button>
-              <button class="button-secondary mt-30">
-                Join Telegram channel
-                <img
-                  src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/telegram_button.svg"
-                  class="ml-2"
-                />
-              </button>
-            </div>
-          </div>
-          <div
-            class="col-lg-6 col-12 landing-page__section--divided-section__right-section"
-          >
-            <div>
-              <div class="heading-font stroke-font text-grey-dark-2">
-                BECOME A
-              </div>
-              <div class="heading-font">SUPERHERO</div>
+              <div class="flex-1 t-align-l">Visit Youtube channel</div>
               <img
-                src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/underline_superhero.svg"
+                src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/youtube_button.svg"
+                class="ml-2"
               />
-              <div class="my-50">
-                <div class="heading-2 bold">Campur Ambassador!</div>
-                <div class="mt-2 text-blue-light-1 heading-4">
-                  Learn and eaarn, all with lots of fun.
-                </div>
+            </button>
+            <button
+              class="button-secondary ml-4 flex-1"
+              style="max-width: fit-content"
+            >
+              <div class="flex-1 t-align-l">Join Telegram channel</div>
+              <img
+                src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/telegram_button.svg"
+                class="ml-2"
+              />
+            </button>
+          </div>
+          <div class="d-xl-none d-lg-block d-sm-none d-block">
+            <button class="button-secondary">
+              Visit Youtube channel
+              <img
+                src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/youtube_button.svg"
+                class="ml-2"
+              />
+            </button>
+            <button class="button-secondary mt-30">
+              Join Telegram channel
+              <img
+                src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/telegram_button.svg"
+                class="ml-2"
+              />
+            </button>
+          </div>
+        </template>
+        <template v-slot:right_section>
+          <div>
+            <div class="heading-font stroke-font text-grey-dark-2">
+              BECOME A
+            </div>
+            <div class="heading-font">SUPERHERO</div>
+            <img
+              src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/underline_superhero.svg"
+            />
+            <div class="my-50">
+              <div class="heading-2 bold">Campur Ambassador!</div>
+              <div class="mt-2 text-blue-light-1 heading-4">
+                Learn and eaarn, all with lots of fun.
               </div>
             </div>
-            <div>
-              <button class="button-secondary">
-                Learn More
-                <img
-                  src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/button-icon.svg"
-                  class="ml-2"
-                />
-              </button>
-            </div>
           </div>
-        </div>
-      </div>
+          <div>
+            <button class="button-secondary">
+              Learn More
+              <img
+                src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/button-icon.svg"
+                class="ml-2"
+              />
+            </button>
+          </div>
+        </template>
+      </DividedSection>
 
       <!-- Events Section -->
       <div class="landing-page__section">
@@ -314,6 +308,7 @@ import WelcomeSection from "@/components/home/WelcomeSection.vue";
 import CardCarousel from "@/components/misc/CardCarousel.vue";
 import EventsCard from "@/components/misc/EventsCard.vue";
 import NewsCard from "@/components/misc/NewsCard.vue";
+import DividedSection from "@/components/misc/DividedSection.vue";
 import CollegesAndCompaniesSection from "@/components/home/CollegesAndCompaniesSection.vue";
 import SuccessStoriesSection from "@/components/home/SuccessStoriesSection.vue";
 import HelpSection from "@/components/home/HelpSection.vue";
@@ -326,6 +321,7 @@ export default {
     CardCarousel,
     EventsCard,
     NewsCard,
+    DividedSection,
     CollegesAndCompaniesSection,
     SuccessStoriesSection,
     HelpSection,

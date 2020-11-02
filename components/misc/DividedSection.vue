14a0dbcc99a92aae904116a468c83d4319af1b64
BipinKalra <bipinkalra@gmail.com>
2020-11-02 19:31:41 +0530

    divided section

--- components/misc/DividedSection.vue
+++ components/misc/DividedSection.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="landing-page__section landing-page__section--divided-section">
-    <div class="row c-card-carousel-lg">
+    <div class="row">
       <div
         class="col-lg-6 col-12 landing-page__section--divided-section__left-section"
       >
         <slot name="left_section"></slot>
       </div>
+      <div class="divider-h d-lg-none d-block my-50"></div>
       <div
         class="col-lg-6 col-12 landing-page__section--divided-section__right-section"
       >

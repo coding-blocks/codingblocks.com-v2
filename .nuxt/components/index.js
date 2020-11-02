c8578ac2e9417aeb18954ab4912e6fa536599285
BipinKalra <bipinkalra@gmail.com>
2020-11-02 18:33:44 +0530

    Divided Section

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -5,6 +5,7 @@ export { default as HelpSection } from '../../components/home/HelpSection.vue'
 export { default as SuccessStoriesSection } from '../../components/home/SuccessStoriesSection.vue'
 export { default as WelcomeSection } from '../../components/home/WelcomeSection.vue'
 export { default as CardCarousel } from '../../components/misc/CardCarousel.vue'
+export { default as DividedSection } from '../../components/misc/DividedSection.vue'
 export { default as EventsCard } from '../../components/misc/EventsCard.vue'
 export { default as NewsCard } from '../../components/misc/NewsCard.vue'
 export { default as StatsCard } from '../../components/misc/StatsCard.vue'
@@ -16,6 +17,7 @@ export const LazyHelpSection = import('../../components/home/HelpSection.vue' /*
 export const LazySuccessStoriesSection = import('../../components/home/SuccessStoriesSection.vue' /* webpackChunkName: "components/home/SuccessStoriesSection" */).then(c => c.default || c)
 export const LazyWelcomeSection = import('../../components/home/WelcomeSection.vue' /* webpackChunkName: "components/home/WelcomeSection" */).then(c => c.default || c)
 export const LazyCardCarousel = import('../../components/misc/CardCarousel.vue' /* webpackChunkName: "components/misc/CardCarousel" */).then(c => c.default || c)
+export const LazyDividedSection = import('../../components/misc/DividedSection.vue' /* webpackChunkName: "components/misc/DividedSection" */).then(c => c.default || c)
 export const LazyEventsCard = import('../../components/misc/EventsCard.vue' /* webpackChunkName: "components/misc/EventsCard" */).then(c => c.default || c)
 export const LazyNewsCard = import('../../components/misc/NewsCard.vue' /* webpackChunkName: "components/misc/NewsCard" */).then(c => c.default || c)
 export const LazyStatsCard = import('../../components/misc/StatsCard.vue' /* webpackChunkName: "components/misc/StatsCard" */).then(c => c.default || c)
---
+++ components/misc/DividedSection.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="landing-page__section landing-page__section--divided-section">
+    <div class="row c-card-carousel-lg">
+      <div
+        class="col-lg-6 col-12 landing-page__section--divided-section__left-section"
+      >
+        <slot name="left_section"></slot>
+      </div>
+      <div
+        class="col-lg-6 col-12 landing-page__section--divided-section__right-section"
+      >
+        <slot name="right_section"></slot>
+      </div>
+    </div>
+  </div>
+</template>

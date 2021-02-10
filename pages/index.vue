943bc576ff40b402791e663eb07fc373312ed346
BipinKalra <bipinkalra@gmail.com>
2021-02-10 15:03:12 +0530

    blogs

--- pages/index.vue
+++ pages/index.vue
@@ -36,138 +36,7 @@
 
       <CollegesAndCompaniesSection />
 
-      <div class="landing-page__section">
-        <div class="width-limiter">
-          <div
-            class="row no-gutters justify-content-between align-items-center"
-          >
-            <div class="flex-1 t-align-sm-l t-align-c">
-              <div class="heading-font stroke-font">
-                LATEST
-              </div>
-              <div class="heading-font">
-                FROM THE BLOG
-              </div>
-              <img
-                src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/underline_blog.svg"
-              />
-            </div>
-            <div
-              class="col-md-3 col-12 mt-md-none mt-40 t-align-md-r talign-l d-sm-block d-none"
-            >
-              <button class="button-secondary">
-                Read All Blogs
-                <img
-                  src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/button-icon-orange.svg"
-                  class="ml-2"
-                />
-              </button>
-            </div>
-          </div>
-          <div class="mt-sm-50 mt-25">
-            <div class="row no-gutters c-card-carousel-md">
-              <div
-                class="col-md-4 col-sm-5 col-8 flex-col justify-content-between blog-post"
-              >
-                <img
-                  src="https://scx1.b-cdn.net/csz/news/800/2018/hack.jpg"
-                  class="w-100"
-                />
-                <div class="mt-2 w-100">
-                  <div class="heading-4 bold mt-4">AI to take over jobs?</div>
-                  <div class="mt-3">
-                    <div
-                      class="heading-6 text-clamp-ellipses text-clamp-ellipses--2"
-                    >
-                      Something just like this is hampering my head space and
-                      fucking up! Something just like this is hampering my head
-                      space and fucking up! Something just like this is
-                      hampering my head space and fucking up! Something just
-                      like this is hampering my head space and fucking up!
-                    </div>
-                  </div>
-                  <div class="button-tertiary mt-50">
-                    Read More
-                    <img
-                      src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/button-icon-orange.svg"
-                      class="ml-2"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div
-                class="col-md-4 col-sm-5 col-8 flex-col justify-content-between blog-post"
-              >
-                <img
-                  src="https://scx1.b-cdn.net/csz/news/800/2018/hack.jpg"
-                  class="w-100"
-                />
-                <div class="mt-2 w-100">
-                  <div class="heading-4 bold mt-4">AI to take over jobs?</div>
-                  <div class="mt-3">
-                    <div
-                      class="heading-6 text-clamp-ellipses text-clamp-ellipses--2"
-                    >
-                      Something just like this is hampering my head space and
-                      fucking up! Something just like this is hampering my head
-                      space and fucking up! Something just like this is
-                      hampering my head space and fucking up! Something just
-                      like this is hampering my head space and fucking up!
-                    </div>
-                  </div>
-                  <div class="button-tertiary mt-50">
-                    Read More
-                    <img
-                      src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/button-icon-orange.svg"
-                      class="ml-2"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div
-                class="col-md-4 col-sm-5 col-8 flex-col justify-content-between blog-post"
-              >
-                <img
-                  src="https://images.newindianexpress.com/uploads/user/imagelibrary/2020/6/8/w900X450/_112604965_ab3d06zxxzccx58-b06c-4475-810b-99d17e6df4c7.jpg"
-                  class="w-100"
-                />
-                <div class="mt-2 w-100">
-                  <div class="heading-4 bold mt-4">AI to take over jobs?</div>
-                  <div class="mt-3">
-                    <div
-                      class="heading-6 text-clamp-ellipses text-clamp-ellipses--2"
-                    >
-                      Something just like this is hampering my head space and
-                      fucking up! Something just like this is hampering my head
-                      space and fucking up! Something just like this is
-                      hampering my head space and fucking up! Something just
-                      like this is hampering my head space and fucking up!
-                    </div>
-                  </div>
-                  <div class="button-tertiary mt-50">
-                    Read More
-                    <img
-                      src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/button-icon-orange.svg"
-                      class="ml-2"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div
-            class="d-sm-none d-block mt-25 row no-gutters justify-content-center"
-          >
-            <button class="button-secondary">
-              Read All Blogs
-              <img
-                src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/button-icon-orange.svg"
-                class="ml-2"
-              />
-            </button>
-          </div>
-        </div>
-      </div>
+      <BlogsSection />
 
       <NewsSection />
 
@@ -184,6 +53,7 @@ import PracticeAndHireSection from "@/components/home/PracticeAndHireSection.vue
 import MentorSection from "@/components/home/MentorSection.vue";
 import CollegesAndCompaniesSection from "@/components/home/CollegesAndCompaniesSection.vue";
 import SuccessStoriesSection from "@/components/home/SuccessStoriesSection.vue";
+import BlogsSection from "@/components/home/BlogsSection.vue";
 import NewsSection from "@/components/home/NewsSection.vue";
 import Footer from "@/components/Footer.vue";
 
@@ -196,6 +66,7 @@ export default {
     MentorSection,
     CollegesAndCompaniesSection,
     SuccessStoriesSection,
+    BlogsSection,
     NewsSection,
     Footer,
   },

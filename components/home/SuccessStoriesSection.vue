67101bed16a4e97899c4f0013df5d0432a562b71
BipinKalra <bipinkalra@gmail.com>
2021-02-11 16:17:42 +0530

    dynamic success stories

--- components/home/SuccessStoriesSection.vue
+++ components/home/SuccessStoriesSection.vue
@@ -17,8 +17,18 @@
       </div>
       <div class="mt-sm-50 mt-25">
         <CardCarousel>
-          <SuccessStory />
-          <SuccessStory />
+          <SuccessStory
+            story_title="Nilesh placed at Apple, 72LPA"
+            story_subtitle="(B.Tech | ME, DTU)"
+            description="Hi, I am Nilesh from Delhi technology university. I have done competitive boot camp from coding blocks. This gave me a great exposure to problem solving mindset and helped me crack Apple interview. Thanks to Coding blocks!"
+            image_link="https://i2.wp.com/digital-photography-school.com/wp-content/uploads/2011/07/outdoor-portraits-1.jpg?resize=2000%2C1160&ssl=1"
+          />
+          <SuccessStory
+            story_title="Bipin placed at Tesla, 90LPA"
+            story_subtitle="(B.Tech | IT, MAIT)"
+            description="Hi, I am Nilesh from Delhi technology university. I have done competitive boot camp from coding blocks. This gave me a great exposure to problem solving mindset and helped me crack Apple interview. Thanks to Coding blocks!"
+            image_link="https://i2.wp.com/digital-photography-school.com/wp-content/uploads/2011/07/outdoor-portraits-1.jpg?resize=2000%2C1160&ssl=1"
+          />
         </CardCarousel>
       </div>
     </div>
--- components/misc/SuccessStory.vue
+++ components/misc/SuccessStory.vue
@@ -6,42 +6,36 @@
       <div class="col-12 d-md-none d-block mb-40">
         <div class="row no-gutters align-items-center">
           <div class="col-sm-6 col-12 mb-sm-none mb-40">
-            <img
-              src="https://i2.wp.com/digital-photography-school.com/wp-content/uploads/2011/07/outdoor-portraits-1.jpg?resize=2000%2C1160&ssl=1"
-              class="success-story__image"
-            />
+            <img :src="image_link" class="success-story__image" />
           </div>
           <div class="flex-1 pl-20">
             <div class="heading-2 bold text-orange mb-3">
-              Nilesh placed at Apple, 72LPA
+              {{ story_title }}
             </div>
-            <div class="heading-4 bold">(B.Tech | ME, DTU)</div>
+            <div class="heading-4 bold">{{ story_subtitle }}</div>
           </div>
         </div>
       </div>
-      <img
-        src="https://i2.wp.com/digital-photography-school.com/wp-content/uploads/2011/07/outdoor-portraits-1.jpg?resize=2000%2C1160&ssl=1"
-        class="success-story__image d-md-block d-none"
-      />
+      <img :src="image_link" class="success-story__image d-md-block d-none" />
       <div class="flex-1">
         <div class="d-md-block d-none">
           <div class="heading-2 bold text-orange mb-3">
-            Nilesh placed at Apple, 72LPA
+            {{ story_title }}
           </div>
           <div class="heading-4 bold mb-40">
-            (B.Tech | ME, DTU)
+            {{ story_subtitle }}
           </div>
         </div>
         <div class="heading-5 text-grey">
-          Hii I am Nilesh from Delhi technology university. I have done
-          competitive boot camp from coding blocks. This gave me a great
-          exposure to problem solving mindset and helped me crack Apple
-          interview.
-          <br />
-          <br />
-          Thanks to Coding blocks!
+          {{ description }}
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: ["story_title", "story_subtitle", "image_link", "description"],
+};
+</script>

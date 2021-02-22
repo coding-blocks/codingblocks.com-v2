3d5bb067baabcb26998e8c0b2a527dd3ac3509ac
BipinKalra <bipinkalra@gmail.com>
2021-02-22 18:37:47 +0530

    small course card

--- components/courses/CourseCardSmall.vue
+++ components/courses/CourseCardSmall.vue
@@ -1,3 +1,21 @@
 <template>
-  
+  <div class="card card--equal-height card--course theme-pink justify-content-center">
+    <div class="card__header card__header--border-highlight mb-none">
+      <div class="flex-1">
+        <div
+          class="s-90x90 br-5 p-15 all-center position-relative card--course__logo theme-background"
+        >
+          <img
+            src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/js_white.svg"
+          />
+          <div class="card--course__logo__tag">Beginner</div>
+        </div>
+      </div>
+      <div class="">
+        <div class="font-2">Starting from</div>
+        <div class="heading-4 bold">&#8377; 9499</div>
+        <del class="font-4 text-grey-light-1">&#8377; 12999</del>
+      </div>
+    </div>
+  </div>
 </template>

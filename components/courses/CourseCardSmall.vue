5dba9265b5509f2e77013ec72ecafd83db05dde4
BipinKalra <bipinkalra@gmail.com>
2021-02-22 18:41:42 +0530

    course card mini done

--- components/courses/CourseCardSmall.vue
+++ components/courses/CourseCardSmall.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="card card--equal-height card--course theme-pink justify-content-center">
+  <div
+    class="card card--equal-height card--course theme-pink justify-content-center"
+  >
     <div class="card__header card__header--border-highlight mb-none">
-      <div class="flex-1">
+      <div>
         <div
           class="s-90x90 br-5 p-15 all-center position-relative card--course__logo theme-background"
         >
@@ -11,10 +13,38 @@
           <div class="card--course__logo__tag">Beginner</div>
         </div>
       </div>
-      <div class="">
-        <div class="font-2">Starting from</div>
-        <div class="heading-4 bold">&#8377; 9499</div>
-        <del class="font-4 text-grey-light-1">&#8377; 12999</del>
+      <div class="flex-1 pl-20">
+        <div class="heading-5 bold theme-color">Python for Devs</div>
+        <div class="row no-gutters align-items-center mt-10">
+          <img
+            src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/star_filled.svg"
+            class="mr-1"
+            style="height: 10px;"
+          />
+          <img
+            src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/star_filled.svg"
+            class="mr-1"
+            style="height: 10px;"
+          />
+          <img
+            src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/star_filled.svg"
+            class="mr-1"
+            style="height: 10px;"
+          />
+          <img
+            src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/star_filled.svg"
+            class="mr-1"
+            style="height: 10px;"
+          />
+          <img
+            src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/star_unfilled.svg"
+            class="mr-1"
+            style="height: 10px;"
+          />
+          <div class="font-1 text-grey ml-2 flex-1">
+            <strong>4.15/5,</strong>&nbsp;65 ratings
+          </div>
+        </div>
       </div>
     </div>
   </div>

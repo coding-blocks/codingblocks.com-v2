da18f3eed633c09dedf762c692179466e5560a23
BipinKalra <bipinkalra@gmail.com>
2021-02-25 18:13:20 +0530

    individual events page

---
+++ pages/events/_id/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="a-cbm" style="margin-top: -70px">
+    <Navigation />
+
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navigation from '@/components/Navigation.vue'
+
+export default {
+  components: {
+    Navigation,
+  },
+}
+</script>

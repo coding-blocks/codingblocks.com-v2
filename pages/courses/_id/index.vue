a08802a2fcd908dc740fa9ac17e7d1d3d0f6f0b3
BipinKalra <bipinkalra@gmail.com>
2021-02-25 21:24:03 +0530

    success story card

--- pages/courses/_id/index.vue
+++ pages/courses/_id/index.vue
@@ -62,36 +62,7 @@
           </div>
           <div class="row mt-50">
             <div class="col-lg-4 col-md-5 col-sm-6 mb-25 h-inherit">
-              <div class="card card--equal-height">
-                <div class="card__header card__header--border-highlight">
-                  <div class="flex-1">
-                    <div class="s-80x80 round">
-                      <img
-                        src="https://minio.codingblocks.com/amoeba/arnva-min.webp"
-                      />
-                    </div>
-                  </div>
-                  <div class="t-align-r">
-                    <div class="font-2 text-grey bold mb-10">Placed at</div>
-                    <img
-                      src="https://minio.codingblocks.com/amoeba/amazon.svg"
-                      style="height: 40px"
-                    />
-                  </div>
-                </div>
-                <div>
-                  <div class="heading-5 bold mb-1">Arnav Gupta</div>
-                  <div class="font-3 text-grey mb-20">
-                    Delhi Tech University
-                  </div>
-                  <div class="font-4">
-                    Coding Blocks was founded in 2014 with a mission to create
-                    skilled software engineers for our country and the world. We
-                    aim to bridge the gap between the quality of skills demanded
-                    by industry and the quality of skills. Read more...
-                  </div>
-                </div>
-              </div>
+              <SuccessStoryCard />
             </div>
           </div>
         </div>
@@ -109,6 +80,7 @@ import CourseProjects from '@/components/courses/CourseProjects.vue'
 import CourseBatch from '@/components/courses/CourseBatch.vue'
 import CourseHighlights from '@/components/courses/CourseHighlights.vue'
 import CoursePageMentorsSection from '@/components/courses/CoursePageMentorsSection.vue'
+import SuccessStoryCard from '@/components/misc/SuccessStoryCard.vue'
 
 export default {
   components: {
@@ -118,6 +90,7 @@ export default {
     CourseBatch,
     CourseHighlights,
     CoursePageMentorsSection,
+    SuccessStoryCard,
   },
 }
 </script>

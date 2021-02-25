5a8d38d101bc923f06d1baa3852c7b69e9576b5e
BipinKalra <bipinkalra@gmail.com>
2021-02-25 20:47:24 +0530

    motley bump

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,39 +5,38 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', type: 'text/css', href: 'https://unpkg.com/@coding-blocks/motley@2.7.17/dist/coding-blocks/app.min.css' }
-    ]
+      {
+        rel: 'stylesheet',
+        type: 'text/css',
+        href:
+          'https://unpkg.com/@coding-blocks/motley@2.8.0/dist/coding-blocks/app.min.css',
+      },
+    ],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-  ],
+  modules: [],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  },
+  build: {},
 
   server: {
-    host: '0.0.0.0'
-  }
-
+    host: '0.0.0.0',
+  },
 }

eb310c2cf9df1642cd63d6e50995eb797b43d08e
BipinKalra <bipinkalra@gmail.com>
2021-03-01 13:22:23 +0530

    images update in universe

--- components/home/Universe.vue
+++ components/home/Universe.vue
@@ -10,51 +10,85 @@
         />
       </div>
       <div v-for="(orbit, index) in orbits" :key="index">
-        <UniverseOrbit 
-          :orbit="orbit"
-        />
+        <UniverseOrbit :orbit="orbit" />
       </div>
     </div>
   </client-only>
 </template>
 
 <script>
-import UniverseOrbit from "@/components/home/UniverseOrbit";
+import UniverseOrbit from '@/components/home/UniverseOrbit'
 
 export default {
   components: {
     UniverseOrbit,
   },
-  data () {
+  data() {
     return {
       orbits: [
         {
           index: 0,
           planets: [
-            "https://news.artnet.com/app/news-upload/2017/10/Taylor-Dafoe_Head-Shot-256x256.jpg",
-            "https://news.artnet.com/app/news-upload/2017/10/Taylor-Dafoe_Head-Shot-256x256.jpg"
-          ]
-        },        
+            [
+              'https://news.artnet.com/app/news-upload/2017/10/Taylor-Dafoe_Head-Shot-256x256.jpg',
+              'https://minio.codingblocks.com/amoeba/arnva-min.webp',
+              'https://minio.codingblocks.com/amoeba/prateek-min.webp',
+            ],
+            [
+              'https://news.artnet.com/app/news-upload/2017/10/Taylor-Dafoe_Head-Shot-256x256.jpg',
+              'https://minio.codingblocks.com/amoeba/arnva-min.webp',
+              'https://minio.codingblocks.com/amoeba/prateek-min.webp',
+            ],
+          ],
+        },
         {
           index: 1,
           planets: [
-            "https://news.artnet.com/app/news-upload/2017/10/Taylor-Dafoe_Head-Shot-256x256.jpg",
-            "https://news.artnet.com/app/news-upload/2017/10/Taylor-Dafoe_Head-Shot-256x256.jpg",
-            "https://news.artnet.com/app/news-upload/2017/10/Taylor-Dafoe_Head-Shot-256x256.jpg"
-          ]
-        },        
+            [
+              'https://news.artnet.com/app/news-upload/2017/10/Taylor-Dafoe_Head-Shot-256x256.jpg',
+              'https://minio.codingblocks.com/amoeba/arnva-min.webp',
+              'https://minio.codingblocks.com/amoeba/prateek-min.webp',
+            ],
+            [
+              'https://news.artnet.com/app/news-upload/2017/10/Taylor-Dafoe_Head-Shot-256x256.jpg',
+              'https://minio.codingblocks.com/amoeba/arnva-min.webp',
+              'https://minio.codingblocks.com/amoeba/prateek-min.webp',
+            ],
+            [
+              'https://news.artnet.com/app/news-upload/2017/10/Taylor-Dafoe_Head-Shot-256x256.jpg',
+              'https://minio.codingblocks.com/amoeba/arnva-min.webp',
+              'https://minio.codingblocks.com/amoeba/prateek-min.webp',
+            ],
+          ],
+        },
         {
           index: 2,
           planets: [
-            "https://news.artnet.com/app/news-upload/2017/10/Taylor-Dafoe_Head-Shot-256x256.jpg",
-            "https://news.artnet.com/app/news-upload/2017/10/Taylor-Dafoe_Head-Shot-256x256.jpg",
-            "https://news.artnet.com/app/news-upload/2017/10/Taylor-Dafoe_Head-Shot-256x256.jpg",
-            "https://news.artnet.com/app/news-upload/2017/10/Taylor-Dafoe_Head-Shot-256x256.jpg"
-          ]
+            [
+              'https://news.artnet.com/app/news-upload/2017/10/Taylor-Dafoe_Head-Shot-256x256.jpg',
+              'https://minio.codingblocks.com/amoeba/arnva-min.webp',
+              'https://minio.codingblocks.com/amoeba/prateek-min.webp',
+            ],
+            [
+              'https://news.artnet.com/app/news-upload/2017/10/Taylor-Dafoe_Head-Shot-256x256.jpg',
+              'https://minio.codingblocks.com/amoeba/arnva-min.webp',
+              'https://minio.codingblocks.com/amoeba/prateek-min.webp',
+            ],
+            [
+              'https://news.artnet.com/app/news-upload/2017/10/Taylor-Dafoe_Head-Shot-256x256.jpg',
+              'https://minio.codingblocks.com/amoeba/arnva-min.webp',
+              'https://minio.codingblocks.com/amoeba/prateek-min.webp',
+            ],
+            [
+              'https://news.artnet.com/app/news-upload/2017/10/Taylor-Dafoe_Head-Shot-256x256.jpg',
+              'https://minio.codingblocks.com/amoeba/arnva-min.webp',
+              'https://minio.codingblocks.com/amoeba/prateek-min.webp',
+            ],
+          ],
         },
-      ]
+      ],
     }
   },
-  props: ["size"],
-};
+  props: ['size'],
+}
 </script>
--- components/home/UniversePlanet.vue
+++ components/home/UniversePlanet.vue
@@ -1,23 +1,35 @@
 <template>
-  <div 
+  <div
     class="universe__orbit__planet"
     :style="{
-      transform: transformStyle
+      transform: transformStyle,
     }"
   >
-    <img
-      :src="planet"
-    />
+    <img :src="planet[counter]" />
   </div>
 </template>
 
 <script>
 export default {
-  props: ["planet", "rotation", "distance"],
+  props: ['planet', 'rotation', 'distance'],
+  data() {
+    return {
+      counter: 0,
+    }
+  },
   computed: {
     transformStyle() {
       return `rotate(${this.rotation}deg) translate(${this.distance}px) rotate(-${this.rotation}deg)`
-    }
-  }
+    },
+  },
+  mounted() {
+    setInterval(() => {
+      if (this.counter < this.planet.length - 1) {
+        this.counter++
+      } else {
+        this.counter = 0
+      }
+    }, 4000)
+  },
 }
 </script>

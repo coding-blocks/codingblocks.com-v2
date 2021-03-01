62a022cbc16a3d8a8aa293745e8aa1410befe47f
BipinKalra <bipinkalra@gmail.com>
2021-03-01 14:34:57 +0530

    testimonials page

---
+++ pages/testimonials/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="a-cbm" style="margin-top: -70px">
+    <Navigation />
+
+    <div class="landing-page">
+      <SuccessStoriesSection />
+
+      <Footer />
+    </div>
+  </div>
+</template>
+
+<script>
+import SuccessStoriesSection from '@/components/home/SuccessStoriesSection.vue'
+
+export default {
+  components: {
+    SuccessStoriesSection,
+  },
+}
+</script>

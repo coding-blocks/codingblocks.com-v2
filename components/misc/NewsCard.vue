42f48ec1d7b33c5d1dfe04b936e0fba2ae48b351
BipinKalra <bipinkalra@gmail.com>
2021-03-03 00:14:29 +0530

    news section dynamic

--- components/misc/NewsCard.vue
+++ components/misc/NewsCard.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="col-6 h-inherit">
     <div class="flex-col justify-content-between h-100 align-items-start">
-      <img :src="news_logo" />
+      <img :src="news.news_logo" :style="{ height: '45px' }" />
       <div>
         <div class="heading-5 my-50 text-clamp-ellipses text-clamp-ellipses--2">
-          {{ news_text }}
+          {{ news.news_text }}
         </div>
-        <a class="button-tertiary" :href="news_link">
+        <a class="button-tertiary" :href="news.news_link">
           Read More
           <img
             src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/button-icon-orange.svg"
@@ -20,6 +20,6 @@
 
 <script>
 export default {
-  props: ["news_logo", "news_text", "news_link"],
-};
+  props: ['news'],
+}
 </script>

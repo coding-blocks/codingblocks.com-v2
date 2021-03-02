aa59c58569807ab64447e9a80d945f32174d53e3
BipinKalra <bipinkalra@gmail.com>
2021-03-03 01:16:10 +0530

    animated career paths text

--- components/home/WelcomeSection.vue
+++ components/home/WelcomeSection.vue
@@ -11,25 +11,19 @@
               App Developer
             </div>
           </div>
-          <div class="heading-font stroke-font">
-            LEARN THE MOST
-          </div>
-          <div class="heading-font">
-            WANTED SKILLS.
-          </div>
+          <div class="heading-font stroke-font">LEARN THE MOST</div>
+          <div class="heading-font">WANTED SKILLS.</div>
           <img
             src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/underline_welcome.svg"
           />
           <div class="mt-30 mb-40">
             <div class="row no-gutters align-items-center">
-              <button class="button-primary">
-                Get Started
-              </button>
+              <button class="button-primary">Get Started</button>
               <div class="ml-30 heading-5 text-grey">Learn. Practice. Ace.</div>
             </div>
           </div>
           <div class="mini-banner position-relative br-5">
-            <div class="bg-purple br-5" style="height: 110px;"></div>
+            <div class="bg-purple br-5" style="height: 110px"></div>
             <div class="mini-banner__tag">
               <div class="font-2">Offer valid till 30th Jan</div>
             </div>
@@ -41,19 +35,15 @@
             <div
               class="d-inline-block text-black underline-highlight underline-highlight--gradient-orange"
             >
-              App Developer
+              {{ goalsList[careerNumber] }}
             </div>
           </div>
-          <div class="heading-font stroke-font">
-            LEARN THE MOST
-          </div>
-          <div class="heading-font">
-            WANTED SKILLS.
-          </div>
+          <div class="heading-font stroke-font">LEARN THE MOST</div>
+          <div class="heading-font">WANTED SKILLS.</div>
           <img
             src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/underline_welcome.svg"
           />
-          <div class="t-align-c" style="margin: 0 -2.5rem;">
+          <div class="t-align-c" style="margin: 0 -2.5rem">
             <Universe size="small" />
             <div class="heading-6 mt-30">
               Helping people get their dream job.
@@ -75,16 +65,14 @@
             <div
               class="row no-gutters align-items-center justify-content-center"
             >
-              <button class="button-primary">
-                Get Started
-              </button>
+              <button class="button-primary">Get Started</button>
               <div class="ml-30 heading-5 text-grey d-xl-block d-none">
                 Learn. Practice. Ace.
               </div>
             </div>
           </div>
           <div class="mini-banner position-relative br-5">
-            <div class="bg-purple br-5" style="height: 110px;"></div>
+            <div class="bg-purple br-5" style="height: 110px"></div>
             <div class="mini-banner__tag">
               <div class="font-2">Offer valid till 30th Jan</div>
             </div>
@@ -105,9 +93,7 @@
 
       <!-- Stats Section -->
       <div class="mt-60 mb-50">
-        <div class="t-align-c heading-3 bold mb-50">
-          Why Coding Blocks
-        </div>
+        <div class="t-align-c heading-3 bold mb-50">Why Coding Blocks</div>
         <div class="row align-items-center justify-content-center">
           <div class="col-xl-3 col-sm-5 col-12 mb-25">
             <StatsCard
@@ -147,15 +133,35 @@
 </template>
 
 <script>
-import StatsCard from "@/components/misc/StatsCard.vue";
-import BannerCarousel from "@/components/misc/BannerCarousel";
-import Universe from "@/components/home/Universe.vue";
+import StatsCard from '@/components/misc/StatsCard.vue'
+import BannerCarousel from '@/components/misc/BannerCarousel'
+import Universe from '@/components/home/Universe.vue'
 
 export default {
+  data() {
+    return {
+      careerNumber: 0,
+      goalsList: [
+        'App Developer',
+        'Web Developer',
+        'Data Scientist',
+        'Competitive Programmer',
+      ],
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      if (this.careerNumber < this.goalsList.length - 1) {
+        this.careerNumber++
+      } else {
+        this.careerNumber = 0
+      }
+    }, 4000)
+  },
   components: {
     StatsCard,
     BannerCarousel,
     Universe,
   },
-};
+}
 </script>

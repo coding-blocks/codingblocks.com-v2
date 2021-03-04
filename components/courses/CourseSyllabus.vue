d25f0d00938ee1297e7a5e6e4acc8f310df3e9c2
BipinKalra <bipinkalra@gmail.com>
2021-03-05 04:12:04 +0530

    course syllabus display dynamically

--- components/courses/CourseSyllabus.vue
+++ components/courses/CourseSyllabus.vue
@@ -13,84 +13,65 @@
       style="max-height: 500px"
     >
       <div style="border-left: solid 2px #f5f5f5">
-        <div class="accordion-container">
-          <div class="p-30 accordion-container__accordion-head">
-            <div
-              class="row no-gutters align-items-center justify-content-between"
-            >
-              <div class="flex-1">
-                <div class="font-4 bold">Network Hardware Systems</div>
-                <div class="font-2 text-grey mt-10">
-                  15 Items | Duration : 60 minutes
+        <div
+          v-for="(content, index) in syllabus.firstSection.contents"
+          :key="index"
+        >
+          <div class="accordion-container">
+            <div class="p-30 accordion-container__accordion-head">
+              <div
+                class="row no-gutters align-items-center justify-content-between"
+              >
+                <div class="flex-1">
+                  <div class="font-4 bold">{{ content.title }}</div>
+                  <div class="font-2 text-grey mt-10">
+                    {{ `${content.videos.length} Items | Duration : ` }}
+                  </div>
                 </div>
+                <img
+                  src="https://minio.codingblocks.com/amoeba/accordion-up.svg"
+                  style="transform: rotate(180deg)"
+                />
               </div>
-              <img
-                src="https://minio.codingblocks.com/amoeba/accordion-up.svg"
-                style="transform: rotate(180deg)"
-              />
+            </div>
+            <div class="accordion-container__accordion-body px-30 pb-10 d-none">
+              <div
+                class="font-2 mb-20"
+                v-for="(video, id) in content.videos"
+                :key="id"
+              >
+                {{ video.title }}
+              </div>
             </div>
           </div>
-          <div class="accordion-container__accordion-body px-30 pb-10">
-            <div class="font-2 mb-20">Some Title</div>
-            <div class="font-2 mb-20">Some Title</div>
-            <div class="font-2 mb-20">Some Title</div>
-          </div>
+          <div class="divider-h content-divider px-30 bg-grey-light-3"></div>
         </div>
-        <div class="divider-h content-divider px-30 bg-grey-light-3"></div>
-
-        <div class="accordion-container">
-          <div class="p-30 accordion-container__accordion-head">
-            <div
-              class="row no-gutters align-items-center justify-content-between"
-            >
-              <div class="flex-1">
-                <div class="font-4 bold">Network Hardware Systems</div>
-                <div class="font-2 text-grey mt-10">
-                  15 Items | Duration : 60 minutes
-                </div>
-              </div>
-              <img
-                src="https://minio.codingblocks.com/amoeba/accordion-up.svg"
-                style="transform: rotate(180deg)"
-              />
-            </div>
-          </div>
-          <div class="accordion-container__accordion-body px-30 pb-10">
-            <div class="font-2 mb-20">Some Title</div>
-            <div class="font-2 mb-20">Some Title</div>
-            <div class="font-2 mb-20">Some Title</div>
-          </div>
-        </div>
-        <div class="divider-h content-divider px-30 bg-grey-light-3"></div>
-
-        <div class="accordion-container">
-          <div class="p-30 accordion-container__accordion-head">
-            <div
-              class="row no-gutters align-items-center justify-content-between"
-            >
-              <div class="flex-1">
-                <div class="font-4 bold">Network Hardware Systems</div>
-                <div class="font-2 text-grey mt-10">
-                  15 Items | Duration : 60 minutes
-                </div>
-              </div>
-              <img
-                src="https://minio.codingblocks.com/amoeba/accordion-up.svg"
-                style="transform: rotate(180deg)"
-              />
-            </div>
-          </div>
-          <div class="accordion-container__accordion-body px-30 pb-10">
-            <div class="font-2 mb-20">Some Title</div>
-            <div class="font-2 mb-20">Some Title</div>
-            <div class="font-2 mb-20">Some Title</div>
-          </div>
-        </div>
-        <div class="divider-h content-divider px-30 bg-grey-light-3"></div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: ['syllabus'],
+  mounted() {
+    document.querySelectorAll('.accordion-container').forEach((accordion) => {
+      const accordionBody = accordion.querySelector(
+        '.accordion-container__accordion-body'
+      )
+      accordion
+        .querySelector('.accordion-container__accordion-head')
+        .addEventListener('click', () => {
+          if (accordionBody.classList.contains('d-none')) {
+            accordionBody.classList.remove('d-none')
+          } else {
+            accordionBody.classList.add('d-none')
+          }
+        })
+    })
+  },
+}
+</script>
 
 <style scoped>
 .accordion-container__accordion-head::after {

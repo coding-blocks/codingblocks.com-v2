d5bd465a7bd43b6b1cacea3578aebc379344be0f
BipinKalra <bipinkalra@gmail.com>
2021-03-05 01:46:03 +0530

    Consultation modal form created

---
+++ components/misc/ConsultationFormModal.vue
@@ -0,0 +1,27 @@
+<template>
+  <ModalForm v-if="showSyllabusModal" @close="setShowSyllabusModal(false)">
+    <ConsultationForm />
+  </ModalForm>
+</template>
+
+<script>
+import ModalForm from '@/components/misc/ModalForm.vue'
+import ConsultationForm from '@/components/misc/ConsultationForm.vue'
+
+export default {
+  components: {
+    ModalForm,
+    ConsultationForm,
+  },
+  data() {
+    return {
+      showSyllabusModal: false,
+    }
+  },
+  methods: {
+    setShowSyllabusModal(value) {
+      this.showSyllabusModal = value
+    },
+  },
+}
+</script>

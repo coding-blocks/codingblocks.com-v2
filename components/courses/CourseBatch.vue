601a0850b717143859e800ff50c27cdb5a58ae00
BipinKalra <bipinkalra@gmail.com>
2021-03-08 19:15:23 +0530

    fix: batches sorted!

--- components/courses/CourseBatch.vue
+++ components/courses/CourseBatch.vue
@@ -2,28 +2,46 @@
   <div class="card">
     <div class="heading-6 bold mb-30">Choose Batch</div>
     <div class="tabs-primary mb-20">
-      <div class="tab active">Noida</div>
-      <div class="tab">Pitampura</div>
+      <div
+        class="tab"
+        v-for="(batch, index) in batches"
+        :key="index"
+        :id="`batch-${index + 1}`"
+      >
+        {{ batch.centre.name.toUpperCase() }}
+      </div>
     </div>
-    <div class="row no-gutters align-items-center">
-      <img
-        src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/classroom-courses-big.svg"
-      />
-      <div class="flex-1 pl-20">
-        <div class="heading-6 bold text-gradient-orange-light">CLASSROOM</div>
-        <div class="mt-2 font-2 text-grey">
-          Batch starting <strong>{{batches[0].startDate}}</strong>
+    <div class="no-gutters c-card-carousel row" id="batches-carousel">
+      <div class="col-12" v-for="(batch, index) in batches" :key="index">
+        <div class="row no-gutters align-items-center">
+          <img
+            src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/classroom-courses-big.svg"
+          />
+          <div class="flex-1 pl-20">
+            <div class="heading-6 bold text-gradient-orange-light">
+              CLASSROOM
+            </div>
+            <div class="mt-2 font-2 text-grey">
+              Batch starting <strong>{{ batch.startDate }}</strong>
+            </div>
+          </div>
         </div>
+        <div class="my-30">
+          <div class="row no-gutters align-items-center">
+            <div class="heading-6 bold mr-10">
+              &#8377;&nbsp;{{ batch.price }}
+            </div>
+            <del class="text-grey bold">&#8377;&nbsp;{{ batch.mrp }}</del>
+          </div>
+          <div class="mt-10 text-grey font-2">
+            Enrollment ends {{ batch.enrollmentEndDate }}
+          </div>
+        </div>
+        <a :href="batch.buyLink" class="button-primary d-block t-align-c">
+          Buy Now
+        </a>
       </div>
     </div>
-    <div class="my-30">
-      <div class="row no-gutters align-items-center">
-        <div class="heading-6 bold mr-10">&#8377;&nbsp;{{batches[0].price}}</div>
-        <del class="text-grey bold">&#8377;&nbsp;{{batches[0].Mrp}}</del>
-      </div>
-      <div class="mt-10 text-grey font-2">Enrollment ends {{batches[0].enrollmentEndDate}}</div>
-    </div>
-    <a :href="batches[0].buyLink" class="button-primary d-block">Buy Now</a>
   </div>
 </template>
 
@@ -31,8 +49,58 @@
 export default {
   props: {
     batches: {
-      type:Array
+      type: Array,
     },
+  },
+  mounted() {
+    const batch_1 = document.querySelector('#batch-1')
+    const batch_2 = document.querySelector('#batch-2')
+    const batch_3 = document.querySelector('#batch-3')
+    const carousel = document.querySelector('#batches-carousel')
+
+    if (batch_1) {
+      batch_1.classList.add('active')
+
+      batch_1.addEventListener('click', () => {
+        carousel.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth',
+        })
+
+        batch_1.classList.add('active')
+        batch_2.classList.remove('active')
+        batch_3.classList.remove('active')
+      })
+    }
+
+    if (batch_2) {
+      batch_2.addEventListener('click', () => {
+        carousel.scrollTo({
+          top: 0,
+          left: carousel.clientWidth,
+          behavior: 'smooth',
+        })
+
+        batch_2.classList.add('active')
+        batch_1.classList.remove('active')
+        batch_3.classList.remove('active')
+      })
+    }
+
+    if (batch_3) {
+      batch_3.addEventListener('click', () => {
+        carousel.scrollTo({
+          top: 0,
+          left: 2 * carousel.clientWidth,
+          behavior: 'smooth',
+        })
+
+        batch_3.classList.add('active')
+        batch_2.classList.remove('active')
+        batch_1.classList.remove('active')
+      })
+    }
   },
 }
 </script>

cd6432b5043f3f09f9f5368948a84beb6a51063f
BipinKalra <bipinkalra@gmail.com>
2021-03-18 16:03:58 +0530

    success stories are sorted now

--- components/testimonials/MajorSuccessStoryCard.vue
+++ components/testimonials/MajorSuccessStoryCard.vue
@@ -9,8 +9,12 @@
             <img :src="story.img" class="success-story__image" />
           </div>
           <div class="flex-1 pl-20">
-            <div class="heading-2 bold text-orange mb-3">
-              {{ story.title }}
+            <div
+              class="heading-2 bold text-orange mb-3 text-clamp-ellipses text-clamp-ellipses--3"
+            >
+              {{
+                `${story.name} placed at ${story.company_name}, ${story.lpa}LPA`
+              }}
             </div>
             <div class="heading-4 bold">{{ story.subtitle }}</div>
           </div>
@@ -18,8 +22,12 @@
       </div>
       <div class="flex-1">
         <div class="d-md-block d-none">
-          <div class="heading-2 bold text-orange mb-3">
-            {{ story.title }}
+          <div
+            class="heading-2 bold text-orange mb-3 text-clamp-ellipses text-clamp-ellipses--3"
+          >
+            {{
+              `${story.name} placed at ${story.company_name}, ${story.lpa}LPA`
+            }}
           </div>
           <div class="heading-4 bold mb-40">
             {{ story.subtitle }}

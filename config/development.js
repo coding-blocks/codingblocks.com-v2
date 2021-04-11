f3ee8117194b66a8948867a94bfc61546d000c56
BipinKalra <bipinkalra@gmail.com>
2021-04-11 19:50:02 +0530

    development env

--- config/development.js
+++ config/development.js
@@ -1,16 +1,16 @@
+// module.exports = {
+//   axios: {
+//     host: '127.0.0.1',
+//     port: 8000,
+//     prefix: '/api',
+//   },
+// }
+
 module.exports = {
   axios: {
-    host: '127.0.0.1',
-    port: 8000,
+    host: 'api.codingblocks.xyz',
+    port: 443,
+    https: true,
     prefix: '/api',
   },
 }
-
-// module.exports = {
-//   axios: {
-//     host: 'api.codingblocks.xyz',
-//     port: 443,
-//     https: true,
-//     prefix: '/api'
-//   }
-// }

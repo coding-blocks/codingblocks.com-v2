14d2998f19a593eae7d7697450304d4ccbd7254c
BipinKalra <bipinkalra@gmail.com>
2021-04-16 16:58:38 +0530

    lgout button

--- components/LoginButton.vue
+++ components/LoginButton.vue
@@ -1,14 +1,23 @@
 <template>
   <client-only>
-    <div v-if="user" :class="hamburgerMenu ? 't-align-c py-20' : ''">
-      {{ user.firstname }}
+    <div v-if="user" :class="hamburgerMenu ? 't-align-c py-20' : 'ml-20'">
+      <a
+        class="user-text position-relative bold font-4 text-gradient-orange"
+        href="https://account.codingblocks.com/logout"
+      >
+        Hi, {{ user.firstname }}
+        <div
+          class="card p-10 font-2 text-grey-light-1 user-text-tooltip bg-white"
+        >
+          Click to Logout
+        </div>
+      </a>
     </div>
     <div v-else :class="hamburgerMenu ? 't-align-c py-20' : ''">
       <a
         href="https://account.codingblocks.com/login"
         class="button-primary"
         :class="hamburgerMenu ? '' : 'ml-20 d-md-block d-none'"
-        target="_blank"
       >
         Login / Signup
       </a>
@@ -40,3 +49,15 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.user-text {
+  position: absolute !important;
+  display: none;
+}
+
+.user-text:hover .user-text-tooltip {
+  display: block;
+  bottom: -80px;
+}
+</style>

2c7243498592df39ea67a9697b7964648d983fcb
Pulkit Aggarwal <aggarwalpulkit596@gmail.com>
2021-04-26 14:05:44 +0530

    add fav logo

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,14 +3,14 @@ const config = require('./config')
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'Coding Blocks - V2',
+    title: 'Coding Blocks',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicons/favicon.ico' },
       {
         rel: 'stylesheet',
         type: 'text/css',
